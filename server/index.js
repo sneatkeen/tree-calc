091fb0dbdaab8cd6b5ae98d6c688a48400e81d4c
sneatkeen <sneatkeen@gmail.com>
2019-09-27 21:58:37 +0200

    Fix file structure

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
 
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3000;
 
 const app = new Koa();
