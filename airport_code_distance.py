eb53e53b224690470658434614598b9a89de032e
Sibasis Dash <43744963+SibasisDash@users.noreply.github.com>
2019-09-28 12:09:29 +0200

    Add files via upload

--- airport_code_distance.py
+++ airport_code_distance.py
@@ -35,6 +35,10 @@ def city_to_airport_code(cityname):
 def distance(lat1,lon1,lat2,lon2):
     # approximate radius of earth in km
     R = 6373.0
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+    lon1 = radians(lon1)
+    lon2 = radians(lon2)
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
